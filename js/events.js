221c2fa83e1b94ccb9f9641f0585a19c1772a668
Jarett Smith <jarettsmith2711@gmail.com>
2018-01-21 18:27:23 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,6 @@ $(document).ready(function(){
 // call functions here
 
 });
+ $('img').on('load') function frameIt(){
+   <img class="tasty" src="https://s3.amazonaws.com/after-school-assets/minion-tongue.jpg">}
+  
